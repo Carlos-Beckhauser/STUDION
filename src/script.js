55d6569e9846954d0de77860bee15c1f5f276024
Carlos <Cbeckhauser@gmail.com>
2022-08-13 11:00:39 -0300

    home responsive finished. Mobile First, desktop second

--- src/script.js
+++ src/script.js
@@ -1,14 +1,26 @@
 $(document).ready(() => {
+  /*MENU MOBILE*/
+
+  $(".navbar__menu-btn").on("click", function () {
+    $(".navbar__links").toggleClass("active");
+    $(this).find("i").toggleClass("fa-bars");
+    $(this).find("i").toggleClass("fa-times");
+  });
+
+  /*CAROUSEL MENU*/
+
   const slickOptions = {
     autoplay: true,
     autoplaySpeed: 3000,
     dots: false,
     prevArrow:
-      '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>',
+      '<button type="button" class="slick-prev slider__prev-arrow slick-arrow">Previous</button>',
     nextArrow:
-      '<button type="button" class="slick-next slider__next-arrow">Next</button>',
+      '<button type="button" class="slick-next slider__next-arrow slick-arrow">Next</button>',
   };
   $(".slider").slick(slickOptions);
+
+  /*CONTADOR AUTOMATICO */
 
   setInterval(() => {
     const numberColletion = document.querySelectorAll(".counter__number");
@@ -25,22 +37,23 @@ $(document).ready(() => {
 
   $(".counter__number").counterUp(counterOptions);
 
+  /*EMAIL NEWSLETTER */
+
   $(".btn--submit").on("Click", () => {
     const email = $("#email").val();
-    const message = "Enviado com sucesso";
 
     Email.send({
-      Host: "smtp.gmail.com",
+      Host: "smtp.elasticemail.com",
       Username: "cbeckhauser@gmail.com",
-      Password: "King&Marduk03",
-      To: "cbeckhauser@gmail.com",
+      Password: "F631841805445527231B69442979FEB76F09",
+      To: "kurotsuchi.ukyru@gmail.com",
       From: email,
       Subject: "Por favor, me adicione na Newsletter",
       Body: `Olá, eu gostaria de ser adicionado na Newsletter do site.
-      Meu Email é ${email}.
-      
-      Obrigado!
-      `,
+          Meu Email é ${email}.
+          
+          Obrigado!
+          `,
     }).then((message) => alert(message));
   });
 });
